cacda7ce669764e76c529d5b312b388dcf9bbe18
superbigbear <lu-ming@outlook.com>
2017-01-10 17:09:14 +0800

    add interceptor and aop settings

---
+++ src/main/java/com/gwz/extension/LoginInterceptor.java
@@ -0,0 +1,40 @@
+package com.gwz.extension;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO calling before action executing
+		//log4
+		 System.out.println("===========HandlerInterceptor preHandle");  
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO calling after action executed ,cut ModelAndView
+		System.out.println("===========HandlerInterceptor postHandle");  
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("===========HandlerInterceptor afterCompletion"); 
+		super.afterCompletion(request, response, handler, ex);
+	}
+
+	public LoginInterceptor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/gwz/extension/TestInterceptor.java
@@ -0,0 +1,21 @@
+package com.gwz.extension;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class TestInterceptor implements MethodInterceptor {
+
+	public TestInterceptor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		// TODO Auto-generated method stub
+		
+		Object returnObject=invocation.proceed();
+		
+		return returnObject;
+	}
+
+}
