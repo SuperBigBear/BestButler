130dd33ba27f23d4e7c195e663741d0e1d219f95
superbigbear <lu-ming@outlook.com>
2017-03-15 14:32:03 +0800

    add dubbo

---
+++ src/main/java/com/gwz/web/api/dubbo/ITestProviderService.java
@@ -0,0 +1,5 @@
+package com.gwz.web.api.dubbo;
+
+public interface ITestProviderService {
+
+}
---
+++ src/main/java/com/gwz/web/api/dubboimpl/TestProviderService.java
@@ -0,0 +1,11 @@
+package com.gwz.web.api.dubboimpl;
+
+import com.gwz.web.api.dubbo.ITestProviderService;
+
+public class TestProviderService implements ITestProviderService {
+
+	public TestProviderService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
